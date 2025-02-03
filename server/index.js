98050fc78d131679618974ecfda9233e3f7068dc
Ridyl <joey.dalatri214@gmail.com>
2025-02-03 15:30:40 -0500

    Back end API works with no issue and previous errors in browser console have been resolved. After trying to redeploy on vercel there is still no react components being rendered

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Allow CORS for frontend
 const allowedOrigins = [
 	process.env.FRONTEND_URL || 'http://localhost:3000',
-	'https://weather-app-delta-livid-35.vercel.app/',
+	'https://weather-4chop2ps4-ridyls-projects.vercel.app/',
 ];
 
 app.use(
