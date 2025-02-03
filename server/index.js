b5771110f98779287814a6bbb04bbe7316d3cc0b
Ridyl <joey.dalatri214@gmail.com>
2025-02-03 13:49:57 -0500

    rewrites

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Allow CORS for frontend
 const allowedOrigins = [
 	process.env.FRONTEND_URL || 'http://localhost:3000',
-	'https://your-vercel-deployed-frontend.vercel.app',
+	'https://weather-app-delta-livid-35.vercel.app/',
 ];
 
 app.use(
