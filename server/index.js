cf595a022391143fb0a535d197df4668088f7729
Ridyl <joey.dalatri214@gmail.com>
2025-02-04 14:42:45 -0500

    Changed cors configuration

--- server/index.js
+++ server/index.js
@@ -17,14 +17,8 @@ const allowedOrigins = [
 
 app.use(
 	cors({
-		origin: function (origin, callback) {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true);
-			} else {
-				callback(new Error('CORS not allowed for this origin'));
-			}
-		},
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		origin: allowedOrigins,
+		methods: ['GET', 'POST'],
 		credentials: true,
 	})
 );
