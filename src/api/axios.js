885ce0d069e07d45355404803cf63f729a094c20
Ridyl <joey.dalatri214@gmail.com>
2025-02-04 13:08:58 -0500

    changing API_URL in axios. Current error is throwing in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL =
-	import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api'; // Deployed backend
+const API_URL = process.env.FRONTEND_URL;
 
 const api = axios.create({
 	baseURL: API_URL,
