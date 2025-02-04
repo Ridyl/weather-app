5a5e4a52b88cc604d2b5b694b51aa5728319537c
Ridyl <joey.dalatri214@gmail.com>
2025-02-04 14:55:55 -0500

    added middleware to ensure API works

--- server/index.js
+++ server/index.js
@@ -25,6 +25,13 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+	next();
+});
+
 // API: Get Quote
 app.get('/api/today', async (req, res) => {
 	try {
